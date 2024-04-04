07fe0dd13f6ae2295b1b74d0baaa13db0e1bac8a
Lexio <alexis.barreyre@gmail.com>
2024-04-04 14:04:42 +0200

    play

--- classes.py
+++ classes.py
@@ -1,12 +1,8 @@
 import random
 
 class Bandit:
-    def __init__(self, avg):
-        self.avg = avg
+    def __init__(self):
+        self.avg = random.gauss(0, 1)
 
-    # determining the values of the parameters 
-    mu = 100
-    sigma = 50
-      
-    # using the gauss() method 
-    print(random.gauss(mu, sigma)) 
+    def play(self):
+        return random.gauss(self.avg, 1)
