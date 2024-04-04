f9f1537f9bb97843a528e3899ad9a7847e4ba5f4
Romain <romain.gojard@gmail.com>
2024-04-04 14:19:23 +0200

    Ajout du graphique Q3

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from classes import Bandit
+import matplotlib.pyplot as plt
+
 bandit = Bandit()
+avg_points = []  # Liste pour stocker les points avg
 
 for i in range(1000):
     bandit.je_veux_une_methode_car_je_suis_relou(bandit.play())
+    avg_points.append(bandit.avg)  # Ajouter avg à la liste
     print(bandit.avg)
+
+# Tracer le graphique
+plt.plot(range(1, 1001), avg_points, label='Avg Points')
+plt.xlabel('i')
+plt.ylabel('Avg Points')
+plt.title('Avg Points vs. i')
+plt.legend()
+plt.show()
