52937c94ff5db6f4b2d80c54f939224ea76ba8c2
Romain <romain.gojard@gmail.com>
2024-04-04 14:58:38 +0200

    Ajout de Bandix

--- classes.py
+++ classes.py
@@ -1,8 +1,17 @@
 import random
 
+
 class Bandit:
     def __init__(self):
         self.avg = random.gauss(0, 1)
 
     def play(self):
         return random.gauss(self.avg, 1)
+
+
+class BanDix:
+    def __init__(self):
+        self.tab = []
+        for i in range(10):
+            self.tab.append(Bandit())
+
