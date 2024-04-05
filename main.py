1b52868befe817482ab86a631c14ef3e8d296b75
Romain <romain.gojard@gmail.com>
2024-04-05 11:49:02 +0200

    Exo4.5

--- main.py
+++ main.py
@@ -2,46 +2,51 @@ from classes import Bandit, GreedyPlayer, BanDix
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-# bandit1, bandit2, bandit3 = Bandit(), Bandit(), Bandit()
-# points1, points2, points3 = [], [], []
-
-
-tabPointsDesGreedy = [0] * 1000
-tableBan10 = []
-tableGreedyP = []
-
+nbTabTabGreedy = 3
 nbOfGreedy = 2000
-tabPourcentageGreedy = [0] * 1000
+tabPointsDesGreedy = [[0] * 1000 for _ in range(nbTabTabGreedy)]
+tableTableBan10 = [[] for _ in range(nbTabTabGreedy)]
+tableTableGreedy = [[] for _ in range(nbTabTabGreedy)]
+tabTabPourcentageGreedy = [[0] * 1000 for _ in range(nbTabTabGreedy)]
 
-for i in range(nbOfGreedy):
-    tableBan10.append(BanDix())
-    tableGreedyP.append(GreedyPlayer(0.0))
-    print([tableBan10[-1].tab[j].avg for j in range(10)])
+for k in range(nbTabTabGreedy):
+    for i in range(nbOfGreedy):
+        tableTableBan10[k].append(BanDix())
+        if k == 0:
+            eps = 0
+        elif k == 1:
+            eps = 0.01
+        else:
+            eps = 0.1
+        tableTableGreedy[k].append(GreedyPlayer(eps))
 
 for i in tqdm(range(1000)):
-    for j in range(nbOfGreedy):
-        action = tableGreedyP[j].get_action()
-        reward = tableBan10[j].play(action)
-        tabPointsDesGreedy[i] += reward / nbOfGreedy
-        tableGreedyP[j].reward(action, reward)
-        if action == tableBan10[j].maxBanditIndex:
-            tabPourcentageGreedy[i] += 1 / 20
+    for k in range(nbTabTabGreedy):
+        for j in range(nbOfGreedy):
+            action = tableTableGreedy[k][j].get_action()
+            reward = tableTableBan10[k][j].play(action)
+            tabPointsDesGreedy[k][i] += reward / nbOfGreedy
+            tableTableGreedy[k][j].reward(action, reward)
+            if action == tableTableBan10[k][j].maxBanditIndex:
+                tabTabPourcentageGreedy[k][i] += 1 / (nbOfGreedy / 100)
 
 # Tracer les deux graphiques
 fig, axs = plt.subplots(2, 1, figsize=(8, 12))
 
 # Premier graphique pour la moyenne des rewards
-axs[0].plot(range(1, 1001), tabPointsDesGreedy, label='Moyenne des rewards des bandits')
+for k in range(nbTabTabGreedy):
+    axs[0].plot(range(1, 1001), tabPointsDesGreedy[k], label=f'Greedy {k+1}')
 axs[0].set_xlabel('i')
 axs[0].set_ylabel('moy reward')
-axs[0].set_title('Moyenne des rewards des bandits vs. i')
+axs[0].set_title('Moyenne des rewards des bandits Greedy vs. i')
 axs[0].legend()
 
 # Deuxième graphique pour le pourcentage
-axs[1].plot(range(1, 1001), tabPourcentageGreedy, label='Pourcentage coups optimaux')
+for k in range(nbTabTabGreedy):
+    axs[1].plot(range(1, 1001), tabTabPourcentageGreedy[k], label=f'Greedy {k+1}')
 axs[1].set_xlabel('i')
 axs[1].set_ylabel('Pourcentage')
-axs[1].set_title('Pourcentage optimaux vs. i')
+axs[1].set_title('Pourcentage de coups optimaux des bandits Greedy vs. i')
 axs[1].legend()
 
 # Ajuster l'espacement entre les graphiques
